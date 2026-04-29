78e18297120a6398709e4c3fd280cd89a48d3652
LaberryO <151982297+LaberryO@users.noreply.github.com>
2026-04-29 13:11:29 +0900

    Delete Python directory

--- Python/Bug-Knights-SE-main/Resource/System/ImageEditor.py
+++
@@ -1,5 +0,0 @@
-from pygame.image import load;
-from pygame.transform import scale;
-
-def rescale(img, size):
-    return scale(load(img), (size, size));
--- Python/Bug-Knights-SE-main/Resource/System/PathLoader.py
+++
@@ -1,7 +0,0 @@
-import os;
-
-def imageLoader(fileName):
-    return os.path.join("Resource", "Image", fileName);
-
-def fontLoader(fontName):
-    return os.path.join("Resource", "Font", fontName);
